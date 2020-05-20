733ca8a0648fba3d0485b4bf14954ea2e7d83abe
Rodrigo Durante <macbookpro@MacBook-Pro-de-MacBook.local>
2020-05-19 20:54:59 -0500

    monsters name

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+        },
+        {
+          name: "Dracula",
+        },
+        {
+          name: "Zombie",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Monster test</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1> {monster.name} </h1>
+        ))}
       </div>
     );
   }
